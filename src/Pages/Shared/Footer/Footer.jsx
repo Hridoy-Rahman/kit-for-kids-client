a7d3c464c953ad85266e9d0abe1f1d46864ebcd7
Hridoy-Rahman <hridoyrahman715@gmail.com>
2023-05-18 13:18:07 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { HiEnvelope, HiPhone, HiMapPin } from "react-icons/hi2";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
     return (
-        <div>
-            this is Footer
-        </div>
+        <footer className=" p-10 bg-base-200 text-base-content flex-row-reverse lg:flex lg:flex-row lg:gap-4 justify-between">
+            <div className='mb-8 '>
+                <img className='h-12' src="https://i.ibb.co/KLj30WC/logo.png" alt="" />
+                <p>Kits For Kids<br />Providing best service</p>
+                <p>Copyright © 2023 - All right reserved by <br /> Kits For Kids</p>
+            </div>
+
+            <div className='mb-8 '>
+                <span className="footer-title">Contact</span>
+                <p className="link link-hover flex items-center gap-2"><HiPhone></HiPhone> 017635423562</p>
+                <p className="link link-hover flex items-center gap-2"><HiEnvelope></HiEnvelope> kfk@gmail.com</p>
+                <p className="link link-hover flex items-center gap-2"><HiMapPin></HiMapPin>Level-4, 34, Awal Centre, Banani, Dhaka</p>
+            </div>
+            <div className='mb-8 '>
+                <span className="footer-title">Follow us on</span>
+                <div className='flex gap-6'>
+                    <a className="link link-hover"><FaFacebook></FaFacebook></a>
+                    <a className="link link-hover"><FaTwitter></FaTwitter></a>
+                    <a className="link link-hover"><FaInstagram></FaInstagram></a>
+                </div>
+            </div>
+            <div className='mb-8 '>
+                <span className="footer-title">Legal</span>
+                <div className='flex flex-col'>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </div>
+        </footer>
     );
 };
 
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            this is navbar
+        <div className="navbar bg-base-100 mt-12 mb-12">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><Link>Home</Link></li>
+                        <li><Link>All Toys</Link></li>
+                        <li><Link>Blog</Link></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="navbar-center">
+                <Link><img className='h-12' src="https://i.ibb.co/KLj30WC/logo.png" alt="" /></Link>
+            </div>
+            <div className="navbar-end">
+                <button className="btn btn-info">Login</button>
+            </div>
         </div>
     );
 };
