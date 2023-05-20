0df06eea0a67bc95ef5afaad859f26c55e21d000
Hridoy-Rahman <hridoyrahman715@gmail.com>
2023-05-20 17:25:26 +0600

    features section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import About from '../About/About';
-import FeatureProduct from '../FeatureProduct/Features';
 import Products from '../Products/Products';
 // import PhotoGallery from '../PhotoGallery/PhotoGallery';
 import Gallery from '../PhotoGallery/PhotoGallery';
+import Features from '../Feature/Features';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <About></About>
-            <Gallery></Gallery>
-            <FeatureProduct></FeatureProduct>
+            <Features></Features>
             <Products></Products>
+            <Gallery></Gallery>
         </div>
     );
 };
